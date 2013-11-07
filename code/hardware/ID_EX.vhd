3942eb2bf5b62e65f5f9c5aa62c6b4aa9604da4a
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2013-11-07 19:30:17 +0100

    Add registers to pipeline separators, fix errors

--- code/hardware/ID_EX.vhd
+++ code/hardware/ID_EX.vhd
@@ -1,29 +1,94 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 use work.mips_constant_pkg.all;
 
 
 entity ID_EX is
+
    generic(DMEM_DATA_BUS : natural := DDATA_BUS; PC_SIZE : natural := MEM_ADDR_COUNT);
     port(
-        pc_in : in numeric_std(PC_SIZE-1 downto 0);
+        clk : in std_logic;
+        reset : in std_logic;
+        halt : in std_logic;
+
+        pc_in : in std_logic_vector(MEM_ADDR_COUNT-1 downto 0);
         immediate_in : in std_logic_vector(DMEM_DATA_BUS-1 downto 0);
         instruction_20_downto_16_in : in std_logic_vector(20 downto 16);
         instruction_15_downto_11_in : in std_logic_vector(15 downto 11);
         rs_data_in : in std_logic_vector(DMEM_DATA_BUS-1 downto 0);
         rt_data_in : in std_logic_vector(DMEM_DATA_BUS-1 downto 0);
         
-        
-        pc_out : out numeric_std(PC_SIZE-1 downto 0);
+        pc_out : out std_logic_vector(MEM_ADDR_COUNT-1 downto 0);
         immediate_out : out std_logic_vector(DMEM_DATA_BUS-1 downto 0);
         instruction_20_downto_16_out : out std_logic_vector(20 downto 16);
         instruction_15_downto_11_out : out std_logic_vector(15 downto 11);
         rs_data_out : out std_logic_vector(DMEM_DATA_BUS-1 downto 0);
-        rt_data_out : out std_logic_vector(DMEM_DATA_BUS-1 downto 0);
+        rt_data_out : out std_logic_vector(DMEM_DATA_BUS-1 downto 0)
     );
 end ID_EX;
 
-
 architecture behavioral of ID_EX is
 begin
+
+    pc_register: entity work.flip_flop
+    generic map(N => MEM_ADDR_COUNT)
+    port map(
+        clk => clk,
+        reset => reset,
+        enable => halt,
+        data_in => pc_in,
+        data_out => pc_out
+    );
+
+    immediate_register: entity work.flip_flop
+    generic map(N => DDATA_BUS)
+    port map(
+        clk => clk,
+        reset => reset,
+        enable => halt,
+        data_in => immediate_in,
+        data_out => immediate_out
+    );
+
+    instruction_20_downto_16_register: entity work.flip_flop
+    generic map(N => 5)
+    port map(
+        clk => clk,
+        reset => reset,
+        enable => halt,
+        data_in => instruction_20_downto_16_in,
+        data_out => instruction_20_downto_16_out
+    );
+
+    instruction_15_downto_11_register: entity work.flip_flop
+    generic map(N => 5)
+    port map(
+        clk => clk,
+        reset => reset,
+        enable => halt,
+        data_in => instruction_15_downto_11_in,
+        data_out => instruction_15_downto_11_out
+    );
+
+    rs_data_register: entity work.flip_flop
+    generic map(N => DDATA_BUS)
+    port map(
+        clk => clk,
+        reset => reset,
+        enable => halt,
+        data_in => rs_data_in,
+        data_out => rs_data_out
+    );
+
+    rt_data_register: entity work.flip_flop
+    generic map(N => DDATA_BUS)
+    port map(
+        clk => clk,
+        reset => reset,
+        enable => halt,
+        data_in => rt_data_in,
+        data_out => rt_data_out
+    );
+
 end behavioral;
--- code/hardware/IF_ID.vhd
+++ code/hardware/IF_ID.vhd
@@ -1,20 +1,54 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 use work.mips_constant_pkg.all;
 
 
 entity IF_ID is
-    generic(IMEM_DATA_BUS : natural := IDATA_BUS; PC_SIZE : natural := MEM_ADDR_COUNT);
+    generic(
+        IMEM_DATA_BUS : natural := IDATA_BUS;
+        PC_SIZE : natural := MEM_ADDR_COUNT
+    );
     port(
-        pc_in : in numeric_std(PC_SIZE-1 downto 0);
-        instruction : in std_logic_vector(IMEM_DATA_BUS-1 downto 0);
-        
-        pc_out : out numberic:_std(PC_SIZE-1 downto 0);
-        instruction : out std_logic_vector(IMEM_DATA_BUS-1 downto 0);
+        clk : in std_logic;
+        reset : in std_logic;
+        halt : in std_logic;
+        pc_in : in unsigned(PC_SIZE-1 downto 0);
+        instruction_in : in std_logic_vector(IMEM_DATA_BUS-1 downto 0);
+
+        pc_out : out unsigned(PC_SIZE-1 downto 0);
+        instruction_out : out std_logic_vector(IMEM_DATA_BUS-1 downto 0)
     );
 end IF_ID;
 
 
 architecture behavioral of IF_ID is
 begin
+
+    main_control_unit: entity work.flip_flop
+    generic map(
+                    OPCODE_SIZE => OPCODE_SIZE,
+                    FUNCTION_SIZE => FUNCTION_SIZE
+                )
+    port map (
+                 reset => reset,
+                 clock => CLK,
+                 instruction_opcode => instruction_opcode,
+                 instruction_func => instruction_func,
+                 processor_enable => processor_enable,
+
+                 register_destination => register_destination,
+                 memory_to_register => memory_to_register,
+                 alu_source => alu_source,
+                 alu_func => alu_func,
+                 register_write => register_write,
+                 jump => jump,
+                 shift_swap => shift_swap,
+                 pc_enable => pc_enable,
+                 memory_write => memory_write
+
+             );
+
+
+
 end behavioral;
