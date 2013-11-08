8c0746488ca7b5fc9ce2dfcf5dede61240b1d3ed
Rune Holmgren <Raane.Holm@gmail.com>
2013-11-08 14:38:05 +0100

    Add a alu with the funtionality we need for basic specs

--- code/hardware/alu.vhd
+++ code/hardware/alu.vhd
@@ -6,16 +6,51 @@ use work.opcodes.all;
 
 
 entity alu is
-    port(
-        x_in : in signed(DDATA_BUS-1 downto 0);
-        y_in : in signed(DDATA_BUS-1 downto 0);
-        function_in : in std_logic_vector(FUNCTION_SIZE-1 downto 0);
-
-        result_out : out signed(DDATA_BUS-1 downto 0);
-        zero_out : out std_logic
-   );
+port(
+		x_in : in signed(DDATA_BUS-1 downto 0);
+		y_in : in signed(DDATA_BUS-1 downto 0);
+		function_in : in std_logic_vector(FUNCTION_SIZE-1 downto 0);
+		
+		result_out : out signed(DDATA_BUS-1 downto 0);
+		zero_out : out std_logic
+    );
 end alu;
 
 architecture behavioural of alu is
 begin
+
+case func is
+when FUNCTION_ADD =>
+r <= x + y;
+
+when FUNCTION_AND =>
+r <= x and y
+
+when FUNCTION_OR =>
+r <= x or y;
+
+when FUNCTION_SLT =>
+if x < y then
+r <= "00000000000000000000000000000001";
+else
+r <= "00000000000000000000000000000000";
+end if;
+
+
+when FUNCTION_SUB =>
+r_readable := x - y;
+r <= r_readable;
+if (r_readable="00000000000000000000000000000000") then
+flags.zero <= '1';
+else
+flags.zero <= '0';
+end if;
+
+when FUNCTION_PASSTHROUGH =>
+r <= x;
+
+when others =>
+null;
+end case;
+
 end behavioural;
