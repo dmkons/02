6eea3f2e53d8e5479ba8da4483018627cd42c792
Rune Holmgren <Raane.Holm@gmail.com>
2013-11-09 17:45:13 +0100

    Completing the alu tb

--- code/tb_alu.vhd
+++ code/tb_alu.vhd
@@ -1,5 +1,6 @@
-LIBRARY ieee;
-USE ieee.std_logic_1164.ALL;
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 use work.test_utils.all;
 use work.opcodes.all;
  
@@ -12,26 +13,24 @@ ARCHITECTURE behavior OF tb_alu IS
  
     COMPONENT alu
     PORT(
-         x_in : IN  std_logic_vector(31 downto 0);
-         y_in : IN  std_logic_vector(31 downto 0);
+         x_in : IN  signed(31 downto 0);
+         y_in : IN  signed(31 downto 0);
          function_in : IN  std_logic_vector(5 downto 0);
-         result_out : OUT  std_logic_vector(31 downto 0);
+         result_out : OUT  signed(31 downto 0);
          zero_out : OUT  std_logic
         );
     END COMPONENT;
     
 
    --Inputs
-   signal x_in : std_logic_vector(31 downto 0) := (others => '0');
-   signal y_in : std_logic_vector(31 downto 0) := (others => '0');
+   signal x_in : signed(31 downto 0) := (others => '0');
+   signal y_in : signed(31 downto 0) := (others => '0');
    signal function_in : std_logic_vector(5 downto 0) := (others => '0');
 
  	--Outputs
-   signal result_out : std_logic_vector(31 downto 0);
+   signal result_out : signed(31 downto 0);
    signal zero_out : std_logic;
-   -- No clocks detected in port list. Replace <clock> below with 
-   -- appropriate port name 
- 
+  
    constant clk_period : time := 10 ns;
  
 BEGIN
@@ -48,8 +47,8 @@ BEGIN
    -- Stimulus process
    stim_proc: process
    begin		
-      wait for 100 ns;	
-      
+        wait for 100 ns;	
+        --Add
         x_in <= "00000000000000000000000000000001";
         y_in <= "00000000000000000000000000000001";
         function_in <= FUNCTION_ADD;
@@ -67,6 +66,104 @@ BEGIN
         function_in <= FUNCTION_ADD;
         wait for clk_period;
         test("Add", "1 + -1", result_out, "00000000000000000000000000000000");
+        wait for clk_period;
+        
+        --And
+        x_in <= "00000000000000000000000000000001";
+        y_in <= "00000000000000000000000000000001";
+        function_in <= FUNCTION_AND;
+        wait for clk_period;
+        test("And", "true and true", result_out, "00000000000000000000000000000001");
+        wait for clk_period;
+        x_in <= "00000000000000000000000000000001";
+        y_in <= "00000000000000000000000000000000";
+        function_in <= FUNCTION_AND;
+        wait for clk_period;
+        test("And", "true and false", result_out, "00000000000000000000000000000000");
+        wait for clk_period;
+        x_in <= "00000000000000000000000000000000";
+        y_in <= "00000000000000000000000000000000";
+        function_in <= FUNCTION_AND;
+        wait for clk_period;
+        test("And", "false and false", result_out, "00000000000000000000000000000000");
+        wait for clk_period;
+        
+        --Or
+        x_in <= "00000000000000000000000000000001";
+        y_in <= "00000000000000000000000000000001";
+        function_in <= FUNCTION_OR;
+        wait for clk_period;
+        test("Or", "true or true", result_out, "00000000000000000000000000000001");
+        wait for clk_period;
+        x_in <= "00000000000000000000000000000001";
+        y_in <= "00000000000000000000000000000000";
+        function_in <= FUNCTION_OR;
+        wait for clk_period;
+        test("Or", "true or false", result_out, "00000000000000000000000000000001");
+        wait for clk_period;
+        x_in <= "00000000000000000000000000000000";
+        y_in <= "00000000000000000000000000000000";
+        function_in <= FUNCTION_OR;
+        wait for clk_period;
+        test("Or", "false or false", result_out, "00000000000000000000000000000000");
+        wait for clk_period;
+        
+        --Slt
+        x_in <= "00000000000000000000000000000001";
+        y_in <= "00000000000000000000000000000001";
+        function_in <= FUNCTION_SLT;
+        wait for clk_period;
+        test("Stl", "1<1", result_out, "00000000000000000000000000000000");
+        wait for clk_period;
+        x_in <= "00000000000000000000000000000001";
+        y_in <= "11111111111111111111111111111111";
+        function_in <= FUNCTION_SLT;
+        wait for clk_period;
+        test("Stl", "1 < -1", result_out, "00000000000000000000000000000000");
+        wait for clk_period;
+        x_in <= "11111111111111111111111111111111";
+        y_in <= "00000000000000000000000000000001";
+        function_in <= FUNCTION_SLT;
+        wait for clk_period;
+        test("Stl", "-1 < 1", result_out, "00000000000000000000000000000001");
+        wait for clk_period;
+        
+        --Sub
+        x_in <= "00000000000000000000000000000001";
+        y_in <= "00000000000000000000000000000001";
+        function_in <= FUNCTION_SUB;
+        wait for clk_period;
+        test("Sub", "1-1", result_out, "00000000000000000000000000000000");
+        test("Sub", "1-1 zero", zero_out, '1');
+        wait for clk_period;
+        x_in <= "11111111111111111111111111111111";
+        y_in <= "11111111111111111111111111111111";
+        function_in <= FUNCTION_SUB;
+        wait for clk_period;
+        test("Sub", "-1 - (-1)", result_out, "00000000000000000000000000000000");
+        test("Sub", "1-1 zero", zero_out, '1');
+        wait for clk_period;
+        x_in <= "00000000000000000000000000000001";
+        y_in <= "11111111111111111111111111111111";
+        function_in <= FUNCTION_SUB;
+        wait for clk_period;
+        test("Sub", "1 - -1", result_out, "00000000000000000000000000000010");
+        test("Sub", "1-1 zero", zero_out, '0');
+        wait for clk_period;
+        x_in <= "11111111111111111111111111111111";
+        y_in <= "00000000000000000000000000000001";
+        function_in <= FUNCTION_SUB;
+        wait for clk_period;
+        test("Sub", "-1 - 1", result_out, "11111111111111111111111111111110");
+        test("Sub", "1-1 zero", zero_out, '0');
+        wait for clk_period;
+        
+        --Passthrough
+        x_in <= "00000000000000000000000000000001";
+        y_in <= "00000000000000000000000000000011";
+        function_in <= FUNCTION_PASSTHROUGH;
+        wait for clk_period;
+        test("Passthrough", "passthrough", result_out, "00000000000000000000000000000011");
         wait for clk_period;
     
       wait for clk_period;
