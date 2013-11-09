1bbd1b34c5e879d939a24f5ce90351103fd5cecf
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2013-11-09 03:44:58 +0100

    Add muxes

---
+++ code/hardware/mux2.vhd
@@ -0,0 +1,29 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use work.mips_constant_pkg.all;
+
+entity mux2 is
+    generic(N : natural);
+    port(
+        a_in : in std_logic_vector(N-1 downto 0);
+        b_in : in std_logic_vector(N-1 downto 0);
+        select_in : in std_logic;
+
+        data_out : out std_logic_vector(N-1 downto 0)
+    );
+end mux2;
+
+
+architecture behavioural of mux2 is
+begin
+
+    process(a_in, b_in, select_in)
+    begin
+        if select_in = '1' then
+            data_out <= a_in;
+        else
+            data_out <= b_in;
+        end if;
+    end process;
+
+end architecture;
